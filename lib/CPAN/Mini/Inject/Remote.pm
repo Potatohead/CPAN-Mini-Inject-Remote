8aae787cf0b3f14b49fc990a5f416e65d746a0f8
Christopher Mckay <potatohead@potatolan.com>
2009-12-22 09:55:38 +1000

    Initial bare commit

---
+++ lib/CPAN/Mini/Inject/Remote.pm
@@ -0,0 +1,106 @@
+package CPAN::Mini::Inject::Remote;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+CPAN::Mini::Inject::Remote - The great new CPAN::Mini::Inject::Remote!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use CPAN::Mini::Inject::Remote;
+
+    my $foo = CPAN::Mini::Inject::Remote->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Christopher Mckay, C<< <potatohead at potatolan.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-cpan-mini-inject-remote at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CPAN-Mini-Inject-Remote>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc CPAN::Mini::Inject::Remote
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=CPAN-Mini-Inject-Remote>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/CPAN-Mini-Inject-Remote>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/CPAN-Mini-Inject-Remote>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/CPAN-Mini-Inject-Remote/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Christopher Mckay.
+
+This program is released under the following license: restrictive
+
+
+=cut
+
+1; # End of CPAN::Mini::Inject::Remote
